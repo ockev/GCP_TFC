0fd5c3ff0a98f85302150d216b685c42afa91a11
ockev <56556870+ockev@users.noreply.github.com>
2023-02-25 13:40:22 -0500

    Update main.tf

--- project-qa/main.tf
+++ project-qa/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-resource "google_compute_instance" "vm_instance" {
+resource "google_compute_instance" "vm_instance1" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
   tags         = ["web", "dev1"]
