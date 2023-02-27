102f247ccc69e722f0f67e7dd91fb3d16638c41c
ockev <koconnor@hashicorp.com>
2023-02-27 10:35:42 -0500

    drift test

--- project-dev/main.tf
+++ project-dev/main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "terraform-instance1"
   machine_type = "f1-micro"
   tags         = ["web", "dev"]
   allow_stopping_for_update = true
